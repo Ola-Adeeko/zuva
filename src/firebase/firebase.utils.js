e3696f9576abe69ca0ee998cbfda171a1f8b6021
Ola Adeeko <ojadeeko@gmail.com>
2022-10-17 07:04:28 +0100

    env variables & oackage manager

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
 const config = {
-  apiKey: "AIzaSyBX1a8BxsL2tEkajM-ugmc9RdAPqfkfJyk",
-  authDomain: "zuva-db.firebaseapp.com",
-  projectId: "zuva-db",
-  storageBucket: "zuva-db.appspot.com",
-  messagingSenderId: "513742032525",
-  appId: "1:513742032525:web:ae7764cb1dd409f9a89cc4",
-  measurementId: "G-FVBLDDFN19",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
